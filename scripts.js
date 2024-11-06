0511faccc27bfea256b19dc840fef2da25731d6b
rabbit1rabbit <147872988+rabbit1rabbit@users.noreply.github.com>
2024-11-06 16:47:36 -0700

    began js work

---
+++ scripts.js
@@ -0,0 +1,6 @@
+sheep.addEventListener('click'function(){
+    document.getElementById("animal").src = "sheep.webp"
+    document.getElementById("animal_bear").style.display="none";
+    document.getElementById("animal_sheep").style.display="initial";
+
+})
