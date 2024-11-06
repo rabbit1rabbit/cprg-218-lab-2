52a571317b056ccde34427c81e1bfc9d2cc008ef
rabbit1rabbit <147872988+rabbit1rabbit@users.noreply.github.com>
2024-11-06 16:57:23 -0700

    final touches
    unable to complete javascript

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
-sheep.addEventListener('click'function(){
+sheep.addEventListener('click',function(){
     document.getElementById("animal").src = "sheep.webp"
     document.getElementById("animal_bear").style.display="none";
     document.getElementById("animal_sheep").style.display="initial";
 
 })
+
+//currently struggling with understanding js//
